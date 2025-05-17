b369f20132b077723f02d12ed1867d2cf5444c12
bogza.anton <bogza.anton@gmail.com>
2025-05-17 01:07:07 -0700

    Add database migration setup using Drizzle ORM
    Introduced migration handling via Drizzle ORM with configurations for PostgreSQL. Updated scripts in `package.json` and added necessary dependencies like `dotenv` and `@neondatabase/serverless`. Included `drizzle.config.ts` and migration script in `lib/db/migrate.ts`.

---
+++ lib/db/migrate.ts
@@ -0,0 +1,25 @@
+import { migrate } from "drizzle-orm/neon-http/migrator";
+import { drizzle } from "drizzle-orm/neon-http"
+import { neon  } from "@neondatabase/serverless";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
+
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is required. (Set in .env/.env.local)");
+}
+
+async function runMigration() {
+    try {
+        const sql = neon(process.env.DATABASE_URL!);
+        const db = drizzle(sql);
+
+        await migrate(db, { migrationsFolder: "./drizzle" });
+        console.log("Migration complete.");
+    } catch {
+        console.log("Migration failed.");
+        process.exit(1);
+    }
+}
+
+await runMigration();
---
+++ lib/db/schema.ts
@@ -0,0 +1,39 @@
+import {pgTable, text, uuid, integer, boolean, timestamp} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
+export const entities = pgTable("entities", {
+    id: uuid("id").defaultRandom().primaryKey(),
+
+    name: text("name").notNull(),
+    path: text("path").notNull(),
+    size: integer("size").notNull(),
+    type: text("type").notNull(),
+
+    fileUrl: text("file_url").notNull(),
+    thumbnailUrl: text("thumbnail_url"),
+
+    userId: text("user_id").notNull(),
+    parentId: uuid("parent_id"),
+
+    isFolder: boolean("is_folder").default(false).notNull(),
+    isFavorite: boolean("is_favorite").default(false).notNull(),
+    isDeleted: boolean("is_deleted").default(false).notNull(),
+
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const entitiesRelations = relations(entities, ({ one, many }) => ({
+    parent: one(entities, {
+        fields: [entities.parentId],
+        references: [entities.id]
+    }),
+
+    children: many(entities)
+}));
+
+export const File = typeof entities.$inferSelect;
+export const NewFile = typeof entities.$inferInsert;
+
+export const Folder = typeof entities.$inferSelect;
+export const NewFolder = typeof entities.$inferInsert;
